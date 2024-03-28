a8cd57265d469588319402c53bf35f96b94699ff
yoonion <yoonion97@gmail.com>
2024-03-28 21:47:02 +0900

    init commit

---
+++ src/main/java/com/example/dockercicd/TestController.java
@@ -0,0 +1,16 @@
+package com.example.dockercicd;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    private static final String version = "v1.0.0";
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello CICD with Docker! version: " + version;
+    }
+}
+
