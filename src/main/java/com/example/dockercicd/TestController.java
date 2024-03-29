e223875980a338fed7080f14ccd7456b7a6f8003
yoonion <yoonion97@gmail.com>
2024-03-30 01:02:55 +0900

    test

--- src/main/java/com/example/dockercicd/TestController.java
+++ src/main/java/com/example/dockercicd/TestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    private static final String version = "v1.0.1";
+    private static final String version = "v1.0.3";
 
     @GetMapping("/")
     public String home() {
